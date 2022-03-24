e47e9d2067a9de1a1fcb851f6ed7d4e556649889
Sheldon <lhanco@gmail.com>
2022-03-24 22:03:12 +0700

    02_CRUD for Activity use CQRS, Mediator, Mapper

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -10,6 +12,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        // ??= means if _mediator is null then HttpContext.RequestServices.GetService<IMediator>()
+        // instead of this --> _mediator ?? (_mediator= HttpContext.RequestServices.GetService<IMediator>());
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
